d1f1f4ea2968b5e24e743d9eaf463ad7de2384a3
Aurélien St <aurelien.strimelle@bstorm.be>
2024-05-31 10:51:53 +0200

    Initialize démo 😎

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Demo 06</h1>
+      <h2>Requete AJAX</h2>
+
+      {/* Formulaire pour chercher le nom d'une gare */}
+      {/* Dashboard de la gare rechercher */}
+      {/* 
+          Composant : 
+           - SearchForm
+           - Dahsboard
+           - Loading
+          Container : 
+           - StationApp
+      */}
+
+    </>
+  )
+}
+
+export default App
